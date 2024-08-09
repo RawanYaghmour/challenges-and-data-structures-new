724bb2fdf49aadb477630c0bac2ee2d3fdf570e9
RawanYaghmour <Rawan.yag02@gmail.com>
2024-08-09 19:49:33 +0300

    Add lab 12

---
+++ Data-Structures/Stack-&-Queue/Stack-&-Queue/MinStack/MinStackClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack___Queue.MinStack
+{
+    public class MinStackClass
+    {
+        private Stack<int> stack;
+        private Stack<int> minStack;
+
+        public MinStackClass()
+        {
+            stack = new Stack<int>();
+            minStack = new Stack<int>();
+        }
+
+        public void Push(int data)
+        {
+            stack.Push(data);
+
+            if (minStack.Count == 0 || data <= minStack.Peek())
+            {
+                minStack.Push(data);
+            }
+        }
+
+        public int Pop()
+        {
+            if (stack.Count == 0) throw new InvalidOperationException("Stack is empty");
+
+            int top = stack.Pop();
+            if (top == minStack.Peek())
+            {
+                minStack.Pop();
+            }
+
+            return top;
+        }
+
+        public int Top()
+        {
+            if (stack.Count == 0) throw new InvalidOperationException("Stack is empty");
+
+            return stack.Peek();
+        }
+
+        public bool IsEmpty()
+        {
+            return stack.Count == 0;
+        }
+
+        public int GetMin()
+        {
+            if (minStack.Count == 0) throw new InvalidOperationException("Stack is empty");
+
+            return minStack.Peek();
+        }
+
+        public void PrintStack()
+        {
+            Console.Write("Top -> ");
+            foreach (var item in stack)
+            {
+                Console.Write(item + " -> ");
+            }
+            Console.WriteLine("null");
+        }
+    }
+}
---
+++ Data-Structures/Stack-&-Queue/Stack-and-Queue-Test/MinStackTest.cs
@@ -0,0 +1,103 @@
+﻿using Stack___Queue.MinStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_and_Queue_Test
+{
+    public class MinStackTest
+    {
+        [Fact]
+        public void TestPushAndMin()
+        {
+            // Arrange
+            var minStack = new MinStackClass();
+
+            // Act & Assert
+            minStack.Push(15);
+
+            // Assert
+            Assert.Equal(15, minStack.GetMin());
+
+            // Act
+            minStack.Push(7);
+
+            // Assert
+            Assert.Equal(7, minStack.GetMin());
+
+            // Act
+            minStack.Push(12);
+
+            // Assert
+            Assert.Equal(7, minStack.GetMin());
+
+            // Act
+            minStack.Push(3);
+
+            // Assert
+            Assert.Equal(3, minStack.GetMin());
+        }
+
+        [Fact]
+        public void TestPopAndMin()
+        {
+            // Arrange
+            var minStack = new MinStackClass();
+            minStack.Push(15);
+            minStack.Push(7);
+            minStack.Push(12);
+            minStack.Push(3);
+
+            // Act & Assert
+            // Act
+            int poppedValue1 = minStack.Pop();
+
+            // Assert
+            Assert.Equal(3, poppedValue1);
+            Assert.Equal(7, minStack.GetMin());
+
+            // Act
+            int poppedValue2 = minStack.Pop();
+
+            // Assert
+            Assert.Equal(12, poppedValue2);
+            Assert.Equal(7, minStack.GetMin());
+        }
+
+        [Fact]
+        public void TestTopAndIsEmpty()
+        {
+            // Arrange
+            var minStack = new MinStackClass();
+            minStack.Push(15);
+            minStack.Push(7);
+
+            // Act & Assert
+            // Act
+            int topValue = minStack.Top();
+
+            // Assert
+            Assert.Equal(7, topValue);
+
+            // Act & Assert
+            // Act
+            bool isEmptyBeforePop = minStack.IsEmpty();
+
+            // Assert
+            Assert.False(isEmptyBeforePop);
+
+            // Act
+            minStack.Pop();
+            minStack.Pop();
+
+            // Act & Assert
+            // Act
+            bool isEmptyAfterPop = minStack.IsEmpty();
+
+            // Assert
+            Assert.True(isEmptyAfterPop);
+        }
+    }
+}
